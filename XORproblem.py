459e66cd54f926aff089dcc259e89303134db2c5
= <luigi.naudi@gmail.com>
2023-11-06 17:31:34 +0100

    Error Checking and bad fact storing into CSV

--- XORproblem.py
+++ XORproblem.py
@@ -1,12 +1,14 @@
 import numpy as np
 import pandas as pd
-#No machine learning libraries
 
 #Loading the Training Data (5XOR split into 80/20 after randomisation and removal of headers)
-training_data = pd.read_csv('C:/Users/luigi/Desktop/Third Year/Business Applications of AI/Assignment/5XOR-Training.csv')
-#print(training_data.head)
-testing_data = pd.read_csv('C:/Users/luigi/Desktop/Third Year/Business Applications of AI/Assignment/5XOR-Testing.csv')
-#print(testing_data.head)
+training_data = pd.read_csv(
+    'C:/Users/luigi/Desktop/Third Year/Business Applications of AI/Assignment/5XOR-Training.csv'
+    )
+
+testing_data = pd.read_csv(
+    'C:/Users/luigi/Desktop/Third Year/Business Applications of AI/Assignment/5XOR-Testing.csv'
+    )
 
 #Sigmoid function for the hidden and output layer neurons
 def sigmoid(x):
@@ -19,19 +21,21 @@ def sigmoid_derivative(x):
 
 #Other Hyperparameters
 learning_rate = 0.2
-epochs = 10000
+epochs = 1000
 input_layer_size = training_data.shape[1]-1
 hidden_layer_size = 4
 output_layer_size = 1
 
-#Training the Model
 #Storing Each set of inputs in an array
-inputs = training_data.iloc[:, :-1].values
-#print(inputs) - 25rX5c matrix
+inputs=training_data.iloc[:, :-1].values
 
 #Storing Each inputs' expected output in an array
 expected_outputs = training_data.iloc[:, -1].values
-#print(expected_outputs) 
+
+#Gathering the Testing Data
+testing = testing_data.iloc[:, :-1].values
+#Gathering the XOR column from the Testing dataset
+testing_ans = testing_data.iloc[:, -1].values
 
 #Seeding numpy.random for reproducability
 np.random.seed(10)
@@ -40,14 +44,45 @@ hidden_weights = np.random.uniform(size=(input_layer_size,hidden_layer_size))
 #Generating random weights for the hidden->output layer
 output_weights = np.random.uniform(size=(hidden_layer_size, output_layer_size))
 
-#Training algorithm 
+error_threshold = 0.2
+good_facts = []
+bad_facts = []
+
+#Training Algorithm 
 for epoch in range(epochs):
     #Feed Forward Propagation
-    #Input Later
+    #Input Layer
     input_feedforward = np.dot(inputs,hidden_weights)
     hidden_layer_input = sigmoid(input_feedforward)
     #Hidden Layer
     hidden_feedforward = np.dot(hidden_layer_input,output_weights)
     output_layer_output = sigmoid(hidden_feedforward)
-       
-print(output_layer_output)
+    
+    #Checking for the error
+    error = expected_outputs.reshape(-1,1) - output_layer_output
+    
+    for i in range(len(error)):
+        fact = {
+            'Epoch': epoch + 1,
+            'Index': i,
+            'Expected Output': expected_outputs[i],
+            'Output': output_layer_output[i][0],
+            'Error': error[i][0]
+        }
+        
+        if abs(error[i]) <= error_threshold:
+            # Save good facts
+            fact['Type'] = 'Good Fact'
+            good_facts.append(fact)
+        else:
+            # Save bad facts
+            fact['Type'] = 'Bad Fact'
+            bad_facts.append(fact)
+
+# Convert lists of dictionaries to Pandas DataFrames
+good_df = pd.DataFrame(good_facts)
+bad_df = pd.DataFrame(bad_facts)
+
+# Save DataFrames as CSV files
+good_df.to_csv('good_facts.csv', index=False)
+bad_df.to_csv('bad_facts.csv', index=False)
